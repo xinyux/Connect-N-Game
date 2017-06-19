9151eaf1090614a4b10a6fdec70e205d0b372159
Xinyu Xie <xinyux@g.ucla.edu>
2017-06-18 22:51:07 -0700

    Initial Commit

---
+++ Project3Version4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project3Version4
+//
+//  Created by Xinyu Xie on 6/18/17.
+//  Copyright © 2017 Olivia. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

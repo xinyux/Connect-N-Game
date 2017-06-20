b56b5ea7a81271531b4494ab2f6e53d8984ad5d7
Olivia <xinyux@g.ucla.edu>
2017-06-19 17:35:58 -0700

    Update main.cpp

--- Project3Version4/main.cpp
+++ Project3Version4/main.cpp
@@ -7,10 +7,10 @@ using namespace std;
 
 int main()
 {
+    // Start a game with a board, size of 4 by 4
+    // 3 checkers in a line gives a winner
     SmartPlayer smart1("smart1");
     HumanPlayer human1("human1");
     Game g1(4, 4, 3, &human1, &smart1);
     g1.play();
-    
-
 }
